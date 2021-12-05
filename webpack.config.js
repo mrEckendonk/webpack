268b6ec0373b91d324249769c4ba23e2baa7dcf4
Eckybrazzz <mike.e@vecks.com.br>
2021-12-05 18:09:44 -0300

    added linter files

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: "development",
+  entry: {
+    index: './src/index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-  }),
-],
-module: {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
+    template: './src/index.html'
+    }),
   ],
- },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 };
